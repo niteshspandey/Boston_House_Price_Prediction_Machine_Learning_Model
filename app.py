b5a7402b3a145275bcecfcbecf8b4d0fd972678f
Nit <niteshspandey08@gmail.com>
2022-09-13 16:50:37 +0530

    Procfile added

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ scaler=pickle.load(open('scaling.pkl','rb'))
 def home():
     return render_template('home.html')
 
-@app.route('/predict',methods=['POST'])
-def predict():
+@app.route('/predict_cli',methods=['POST'])
+def predict_cli():
     data=request.json['data']
     print(data)
     print(np.array(list(data.values())).reshape(1,-1))
@@ -19,6 +19,15 @@ def predict():
     output=reg_model.predict(new_data)
     print(output[0])
     return jsonify(output[0])
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=scaler.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=reg_model.predict(final_input)
+    return render_template("home.html",prediction_text="The house price prediction is {}".format(output))
+
 
 if __name__=="__main__":
     app.run(debug=True)
